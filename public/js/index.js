42339a8dc3b35202521922659592bee795a2c16f
Jon Woo <jw56578@gmail.com>
2019-03-05 15:48:13 -0600

    redux

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,6 @@ function shoppingCart(state, action) {
     if (typeof state === 'undefined') {
       return []
     }
-  
     switch (action.type) {
       case 'ADD_PRODUCT_TO_CART':
         return [...state, action.value];
@@ -32,6 +31,3 @@ function products(state, action) {
 ReactDOM.render(
     <App />
     ,document.getElementById("root"));
-
-    "use strict";
-
